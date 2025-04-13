b3c688954c8b98c3ffa236a7b0d7f17797686fd2
Harinov_Ivan <harinovivan410@gmail.com>
2025-04-13 19:20:28 +0300

    сделано дз

---
+++ lesson-js/example.js
@@ -0,0 +1,11 @@
+let user_name = prompt("Введите ваше имя");
+let user_email = print("Введите пожалуйста почту");
+console.log("Ваше имя", user_name);
+console.log("Ваша почта", user_email);
+let confirm = confirm("Вы подтвержадите данные?")
+if(confirm){
+    console.log("Данные подтверждены");
+} 
+else {
+    console.log("Ошибка");
+}
